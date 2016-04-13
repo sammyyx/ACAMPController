e258a0f17829fd7a80470ee335116cd16e210dac
sammy <sammyyaoxin@gmail.com>
2016-04-13 20:51:35 +0800

    Merge

--- src/main/java/net/floodlightcontroller/packet/ACAMPMsgEle.java
+++ src/main/java/net/floodlightcontroller/packet/ACAMPMsgEle.java
@@ -39,13 +39,11 @@ public class ACAMPMsgEle extends BasePacket implements IPacket {
 			payload.setParent(this);
 			payloadData = payload.serialize();
 		}
-		this.messageElementLength = (short) ((payloadData == null) ? 
-				0 : payloadData.length);
 		byte[] data = new byte[this.messageElementLength + ELE_HEADER_LEN];
-		ByteBuffer bb = ByteBuffer.wrap(payloadData);
+		ByteBuffer bb = ByteBuffer.wrap(data);
 		bb.putShort(messageElementType);
 		bb.putShort(messageElementLength);
-		if(payloadData.length != 0) {
+		if(payloadData != null) {
 			bb.put(payloadData);
 		}
 		return data;
